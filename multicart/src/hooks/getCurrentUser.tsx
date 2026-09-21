38f563c4f22a7208803e8edd86b54901e1c522c1
Ayush Kant <ayushkant2807@gmail.com>
2026-09-22 00:44:53 +0530

    Make current-user hydration authentication-aware

--- multicart/src/hooks/getCurrentUser.tsx
+++ multicart/src/hooks/getCurrentUser.tsx
@@ -1,27 +1,33 @@
-'use client'
-import { AppDispatch } from '@/redux/store'
-import { setUserData } from '@/redux/userSlice'
-import axios from 'axios'
-import { useSession } from 'next-auth/react'
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+"use client";
+
+import { AppDispatch } from "@/redux/store";
+import { setUserData } from "@/redux/userSlice";
+import axios from "axios";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 function getCurrentUser() {
-    const dispatch = useDispatch<AppDispatch>()
-    const { status } = useSession()
-useEffect(()=>{
-    const fetchUser = async ()=>{
-        try {
-            const result = await axios.get("/api/currentUser")
-            dispatch(setUserData(result.data))
-        } catch (error) {
-            console.log(error)
-            dispatch(setUserData(null))
-        }
+  const dispatch = useDispatch<AppDispatch>();
+  const { status } = useSession();
 
+  useEffect(() => {
+    if (status !== "authenticated") {
+      dispatch(setUserData(null));
+      return;
     }
-    fetchUser()
-},[status, dispatch])
+
+    const fetchUser = async () => {
+      try {
+        const result = await axios.get("/api/currentUser");
+        dispatch(setUserData(result.data));
+      } catch {
+        dispatch(setUserData(null));
+      }
+    };
+
+    fetchUser();
+  }, [status, dispatch]);
 }
 
-export default getCurrentUser
+export default getCurrentUser;
