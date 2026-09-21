1bfcf3a7f774a42c08b9ffbca6d0caadd60772b7
Ayush Kant <ayushkant2807@gmail.com>
2026-09-22 00:46:45 +0530

    Route product hydration through public or privileged catalog API

--- multicart/src/hooks/getAllProductsData.tsx
+++ multicart/src/hooks/getAllProductsData.tsx
@@ -1,29 +1,41 @@
-'use client'
-import { AppDispatch, RootState } from '@/redux/store'
-import { setAllProductsData } from '@/redux/vendorSlice'
+"use client";
 
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { AppDispatch, RootState } from "@/redux/store";
+import { setAllProductsData } from "@/redux/vendorSlice";
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 function getAllProductsData() {
-    const dispatch = useDispatch<AppDispatch>()
-    const {userData} = useSelector(
-        (state: RootState) => state.user
-      );
-useEffect(()=>{
-    const fetchAllProducts = async ()=>{
-        try {
-            const result = await axios.get("/api/product/all-products-data")
-            dispatch(setAllProductsData(result.data))
-        } catch (error) {
-            console.log(error)
-            dispatch(setAllProductsData([]))
-        }
+  const dispatch = useDispatch<AppDispatch>();
+  const { userData } = useSelector(
+    (state: RootState) => state.user
+  );
 
-    }
-    fetchAllProducts()
-},[userData])
+  useEffect(() => {
+    const fetchAllProducts = async () => {
+      try {
+        const endpoint =
+          userData?.role === "admin" || userData?.role === "vendor"
+            ? "/api/product/all-products-data"
+            : "/api/product/public-products";
+
+        const result = await axios.get(endpoint);
+
+        dispatch(
+          setAllProductsData(
+            Array.isArray(result.data)
+              ? result.data
+              : result.data.products || []
+          )
+        );
+      } catch {
+        dispatch(setAllProductsData([]));
+      }
+    };
+
+    fetchAllProducts();
+  }, [userData?.role, dispatch]);
 }
 
-export default getAllProductsData
+export default getAllProductsData;
