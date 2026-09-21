44aa9e4472a2fe76bbe481fbb67148bf16385aac
Ayush Kant <ayushkant2807@gmail.com>
2026-09-22 00:45:06 +0530

    Strengthen NextAuth session typing

--- multicart/src/next-auth.d.ts
+++ multicart/src/next-auth.d.ts
@@ -1,9 +1,15 @@
 declare module "next-auth" {
-    interface User {
-        id:string,
-        name:string,
-        email:string,
-        role:string
-    }
+  interface User {
+    id: string;
+    name: string;
+    email: string;
+    image?: string;
+    role: "user" | "vendor" | "admin";
+  }
+
+  interface Session {
+    user: User;
+  }
 }
-export {}
+
+export {};
