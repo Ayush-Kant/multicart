4581d31c1cbc3145fd9fd7dc4ec4f8f3e891a478
Ayush Kant <ayushkant2807@gmail.com>
2026-09-22 00:47:53 +0530

    Add vendor role gate import

--- multicart/src/app/add-vendor-product/page.tsx
+++ multicart/src/app/add-vendor-product/page.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { FiUpload } from "react-icons/fi";
 import axios from "axios";
+import RoleGate from "@/component/auth/RoleGate";
 
 const categories = [
   "Fashion & Lifestyle",
